081baf43d45942908d0f8de58b59903f5e779909
Anton Gorobets <a.p.gorobets@gmail.com>
2020-07-23 14:47:31 +0300

    checker almost work

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -31,14 +31,15 @@
 ** Stack Overflow - https://vk.cc/aiVRsC
 ** https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit
 ** Templates to use colors:
-** # define RED "\e[1;31m"
-** # define GRN "\e[1;32m"
-** # define YEL "\e[1;33m"
-** # define BLU "\e[1;34m"
-** # define MAG "\e[1;35m"
-** # define CYN "\e[1;36m"
-** # define END "\e[m"
 */
+# define RED "\e[1;31m"
+# define GRN "\e[1;32m"
+# define YEL "\e[1;33m"
+# define BLU "\e[1;34m"
+# define MAG "\e[1;35m"
+# define CYN "\e[1;36m"
+# define END "\e[m"
+
 
 /*
 ** --------------------------- Macros \ Static ---------------------------------
